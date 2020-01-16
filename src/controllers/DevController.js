02362b96dea9ca7f02869713ee496d04f5229259
Remulo Costa <remulo.costa@gmail.com>
2020-01-16 01:04:53 -0300

    metodo update dev

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -44,8 +44,38 @@ module.exports = {
     return response.json(dev);
   },
 
-  async update() {
+  async update(request, response) {
     // Name, avatar, localização, tecnologias.
+    const { id } = request.params;
+    const { name, avatar_url, techs, latitude, longitude } = request.body;
+
+    const techsArray = parseStringAsArray(techs);
+
+    const location = {
+      type: 'Point',
+      coordinates: [longitude, latitude],
+    };
+
+    try {
+      const dev = await Dev.findByIdAndUpdate(
+        id,
+        {
+          name,
+          avatar_url,
+          techs: techsArray,
+          location,
+        },
+        { new: true }
+      );
+
+      if (!dev) {
+        throw 'Dev informado não foi localizado';
+      }
+
+      return response.json(dev);
+    } catch (err) {
+      return response.status(400).json({ Error: err });
+    }
   },
 
   async destroy() {},
--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,8 @@ const routes = Router();
 
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+routes.put('/devs/:id', DevController.update);
+routes.delete('/devs/:id', DevController.destroy);
 
 routes.get('/search', SearchController.index);
 
